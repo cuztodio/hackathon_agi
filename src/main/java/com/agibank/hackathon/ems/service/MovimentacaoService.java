b8a9c655646688d35fa236f28d3ad786fee439cf
pucheranni <108954830+pucheranni@users.noreply.github.com>
2025-04-28 18:59:38 -0300

    Create MovimentacaoService.java

---
+++ src/main/java/com/agibank/hackathon/ems/service/MovimentacaoService.java
@@ -0,0 +1,26 @@
+@Service
+@RequiredArgsConstructor
+public class MovimentacaoService {
+
+    private final MovimentacaoRepository movimentacaoRepository;
+
+    public Movimentacao createMovement(Movimentacao movimentacao) {
+        movimentacao.setDataMovimentacao(LocalDateTime.now());
+        movimentacao.setStatus(StatusMovimentacao.EMPRESTADO);
+        return movimentacaoRepository.save(movimentacao);
+    }
+
+    public Movimentacao finalizeMovement(String movementId) {
+        return movimentacaoRepository.findById(movementId)
+                .map(m -> {
+                    m.setDataDevolucao(LocalDateTime.now());
+                    m.setStatus(StatusMovimentacao.DEVOLVIDO);
+                    return movimentacaoRepository.save(m);
+                })
+                .orElseThrow(() -> new RuntimeException("Movement not found"));
+    }
+
+    public List<Movimentacao> getEmployeeHistory(String employeeId) {
+        return movimentacaoRepository.findByFuncionarioId(employeeId);
+    }
+}
