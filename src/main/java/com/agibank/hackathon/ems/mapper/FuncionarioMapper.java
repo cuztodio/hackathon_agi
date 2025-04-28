e89450c6f4755eb709d26d320717c5c5c9901557
Victoria Rocha <victoria_rocha_26@hotmail.com>
2025-04-28 16:30:55 -0300

    Feat: Mapper Editar Funcionario

--- src/main/java/com/agibank/hackathon/ems/mapper/FuncionarioMapper.java
+++ src/main/java/com/agibank/hackathon/ems/mapper/FuncionarioMapper.java
@@ -1,5 +1,5 @@
 package com.agibank.hackathon.ems.mapper;
-import com.agibank.hackathon.ems.controller.request.CriarFuncionarioRequest;
+import com.agibank.hackathon.ems.controller.request.*;
 import com.agibank.hackathon.ems.entity.Funcionario;
 import com.agibank.hackathon.ems.enums.StatusFuncionario;
 import jakarta.validation.Valid;
@@ -15,4 +15,15 @@ public class FuncionarioMapper {
                 .status(StatusFuncionario.ATIVO)
                 .build();
     }
+
+    public Funcionario editarFuncionario (String id, @Valid EditarFuncionarioRequest request){
+        return Funcionario.builder()
+                .id(id)
+                .nome(request.getNome())
+                .cpf(request.getCpf())
+                .status(request.getStatus())
+                .build();
+    }
+
+
 }
