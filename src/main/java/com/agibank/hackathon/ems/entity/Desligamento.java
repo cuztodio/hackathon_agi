61224da70159bdd5ffe3e07b801adca2b6a8ddda
rodrigo.nogueira <rodrigosdn94@gmail.com>
2025-04-28 16:41:28 -0300

    correção do date

--- src/main/java/com/agibank/hackathon/ems/entity/Desligamento.java
+++ src/main/java/com/agibank/hackathon/ems/entity/Desligamento.java
@@ -1,14 +1,14 @@
 package com.agibank.hackathon.ems.entity;
 
 import com.agibank.hackathon.ems.enums.StatusDesligamento;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import java.util.Date;
 
 @Data
 public class Desligamento {
     @Id
     private String idFuncionario;
     private StatusDesligamento statusDesligamento;
-    private date dataDesligamento;
+    private Date dataDesligamento;
 }
