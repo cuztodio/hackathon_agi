8cd19e3bbf19ed5c3aeefe165345988c0a5f817c
pedro.custodio <custodiou07@gmail.com>
2025-04-28 22:22:40 -0300

    requisiçoes http para a classe movimentacao no controller

--- src/main/java/com/agibank/hackathon/ems/controller/MovimentacaoController.java
+++ src/main/java/com/agibank/hackathon/ems/controller/MovimentacaoController.java
@@ -1,14 +1,15 @@
 package com.agibank.hackathon.ems.controller;
 
 import com.agibank.hackathon.ems.controller.request.movimentacao.CriarMovimentacaoRequest;
+import com.agibank.hackathon.ems.controller.request.movimentacao.EditarMovimentacaoRequest;
+import com.agibank.hackathon.ems.entity.Movimentacao;
 import com.agibank.hackathon.ems.repository.MovimentacaoRepository;
 import com.agibank.hackathon.ems.service.MovimentacaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/movimentacao")
 @RestController
@@ -21,8 +22,26 @@ public class MovimentacaoController {
     private MovimentacaoRepository movimentacaoRepository;
 
     @PostMapping
-    public ResponseEntity<String> criarMovimentacao(@RequestBody CriarMovimentacaoRequest criarMovimentacaoRequest){
+    public ResponseEntity<String> solitacaoDeEquipamento(@RequestBody CriarMovimentacaoRequest criarMovimentacaoRequest){
         movimentacaoService.criarMovimento(criarMovimentacaoRequest);
         return ResponseEntity.ok("Movimento criado!");
+    }
+
+    @PatchMapping("/{movimentoId}/devolucao")
+    public ResponseEntity<Movimentacao> solicitarDevolucao(@PathVariable String movimentoId) {
+        Movimentacao devolvida = movimentacaoService.solicitarDevolucao(movimentoId);
+        return ResponseEntity.ok(devolvida);
+    }
+
+    @PutMapping("/{id}/editarMovimentacao")
+    public ResponseEntity<String> alteracaoDeMovimentacao(@PathVariable String id, @RequestBody EditarMovimentacaoRequest editarMovimentacaoRequest){
+        movimentacaoService.editarMovimentacao(id, editarMovimentacaoRequest);
+        return ResponseEntity.ok("Movimentacao alterada");
+    }
+
+    @GetMapping("/movimentacao/{id}")
+    public ResponseEntity<List<Movimentacao>> mostrarMovimentacoes(@PathVariable String id){
+        List<Movimentacao> historico = movimentacaoService.getHistoricoFuncionario(id);
+        return ResponseEntity.ok(historico);
     }
 }
