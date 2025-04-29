eba1940aca3528b821b3ba9ff44813eeb7640224
geovanna.melo <geovannabeatrizam@gmail.com>
2025-04-29 00:08:56 -0300

    testes

--- src/main/java/com/agibank/hackathon/ems/repository/EquipamentoRepository.java
+++ src/main/java/com/agibank/hackathon/ems/repository/EquipamentoRepository.java
@@ -1,17 +1,15 @@
 package com.agibank.hackathon.ems.repository;
 
 import com.agibank.hackathon.ems.entity.Equipamentos;
-import com.agibank.hackathon.ems.entity.Movimentacao;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Repository
 public interface EquipamentoRepository extends MongoRepository <Equipamentos,String> {
 
-    List<Equipamentos> findByidEquipamentos(String idEquipamentos);
+    List<Equipamentos> findByIdEquipamento(String idEquipamentos);
 
         List<Equipamentos> findBySku(String sku);
 }
