90af4ad46a6e493fdbd00dfe763085a32932dc31
leandro.sena <leandro.rocha.sena@gmail.com>
2025-04-28 23:51:15 -0300

    Controller

---
+++ src/main/java/com/agibank/hackathon/ems/exceptions/EquipamentoNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.agibank.hackathon.ems.exceptions;
+
+public class EquipamentoNaoEncontradoException extends RuntimeException {
+    public EquipamentoNaoEncontradoException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/agibank/hackathon/ems/repository/EquipamentoRepository.java
+++ src/main/java/com/agibank/hackathon/ems/repository/EquipamentoRepository.java
@@ -1,9 +1,17 @@
 package com.agibank.hackathon.ems.repository;
 
 import com.agibank.hackathon.ems.entity.Equipamentos;
+import com.agibank.hackathon.ems.entity.Movimentacao;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Repository
 public interface EquipamentoRepository extends MongoRepository <Equipamentos,String> {
+
+    List<Equipamentos> findByidEquipamentos(String idEquipamentos);
+
+        List<Equipamentos> findBySku(String sku);
 }
